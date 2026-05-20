6c5f856587a86bf9d3a565545d60b51ce585d588
MayankHenry <mayank.gla_cs.aiml24@gla.ac.in>
2026-05-20 20:56:13 +0530

    updated frontend api url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 // base url for all api calls — change this when deploying
-const API_BASE = "https://primetrade-backend-assignment-4lth.onrender.com";
+const API_BASE = "https://primetrade-backend-assignment-4lth.onrender.com/api/v1";
